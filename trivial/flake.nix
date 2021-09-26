0696506fe34a52d855b1c377dfcfc165aa8f0ebd
Winter <winter@winter.cafe>
2021-09-26 16:39:55 -0400

    trivial: use upstream flake-utils

--- trivial/flake.nix
+++ trivial/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:winterqt/flake-utils";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
     let
